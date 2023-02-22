240ca163be08f12209f179358c755ae5847912db
pgianferro <pablogianferro@gmail.com>
2023-02-21 21:48:24 -0300

    password fixes

--- atlas_url.js
+++ atlas_url.js
@@ -1,8 +1,8 @@
 
-module.exports = uri = "mongodb+srv://admin:lSeqKDH2waYq7vm2@myatlasclusteredu.8riisqd.mongodb.net/?retryWrites=true&w=majority";
+module.exports = uri = "mongodb+srv://admin:password@myatlasclusteredu.8riisqd.mongodb.net/?retryWrites=true&w=majority";
 
 //const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://admin:lSeqKDH2waYq7vm2@myatlasclusteredu.8riisqd.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://admin:password@myatlasclusteredu.8riisqd.mongodb.net/?retryWrites=true&w=majority";
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
